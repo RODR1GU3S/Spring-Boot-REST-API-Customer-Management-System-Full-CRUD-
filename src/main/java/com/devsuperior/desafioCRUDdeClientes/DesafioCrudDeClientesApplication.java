fd1b1e4d35df50c680a90e3e491bb1ac7f89e750
RODR1GU3S <ronaldo.r.silva.78@gmail.com>
2025-11-28 17:56:59 -0300

    Entities and Seending

---
+++ src/main/java/com/devsuperior/desafioCRUDdeClientes/DesafioCrudDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioCRUDdeClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudDeClientesApplication.class, args);
+	}
+
+}
