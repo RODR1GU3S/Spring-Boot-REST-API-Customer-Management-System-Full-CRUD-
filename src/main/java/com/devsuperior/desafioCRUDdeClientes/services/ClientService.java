dd2aac859c60a973b0234af7dd06a7c510b94715
RODR1GU3S <ronaldo.r.silva.78@gmail.com>
2025-12-04 18:14:51 -0300

    Método Put and Delete

--- src/main/java/com/devsuperior/desafioCRUDdeClientes/services/ClientService.java
+++ src/main/java/com/devsuperior/desafioCRUDdeClientes/services/ClientService.java
@@ -16,27 +16,44 @@ public class ClientService {
     private ClientRepository repository;
 
     @Transactional(readOnly = true)
-    public ClientDTO findById(Long id) {
-        Client client = repository.findById(id).get();
-        return new ClientDTO(client);
-    }
-
-    @Transactional(readOnly = true)
     public Page<ClientDTO> findAll(Pageable pageable) {
         Page<Client> result = repository.findAll(pageable);
         return result.map(x-> new ClientDTO(x));
     }
 
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+
     @Transactional
     public ClientDTO insert(ClientDTO dto) {
         Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
         entity.setIncome(dto.getIncome());
         entity.setBirthDate(dto.getBirthDate());
         entity.setChildren(dto.getChildren());
-        entity = repository.save(entity);
-        return new ClientDTO(entity);
     }
 
 }
